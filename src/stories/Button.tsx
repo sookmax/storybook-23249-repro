7d07fe6bcb54c4d68840e6c561529a27af6905c0
sook <sukkyu.chung@gmail.com>
2023-06-30 22:48:05 +0900

    yarn seems broken & add more types

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./button.css";
 
-interface ButtonProps {
+interface ButtonProps extends React.ComponentPropsWithRef<"button"> {
   /**
    * Is this the principal call to action on the page?
    */
@@ -27,14 +27,8 @@ interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button = React.forwardRef(
-  ({
-    primary = false,
-    size = "medium",
-    backgroundColor,
-    label,
-    ...props
-  }: ButtonProps) => {
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ primary = false, size = "medium", backgroundColor, label, ...props }) => {
     const mode = primary
       ? "storybook-button--primary"
       : "storybook-button--secondary";
